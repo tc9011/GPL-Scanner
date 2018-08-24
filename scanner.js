a5d6c68fb2176989021f1d29d26b7660a1ad99b0
tc9011 <tc9011@126.com>
2018-08-25 00:43:49 +0800

    fix(app): delete results.txt at first

--- scanner.js
+++ scanner.js
@@ -1,34 +1,52 @@
 const fs = require('fs');
 const path = require('path');
 
-//解析需要遍历的文件夹
+// 解析需要遍历的文件夹
 const filePath = path.resolve('./node_modules');
+
+// 存储扫描结果
 let results = '';
 
-fileDisplay(filePath);
+fs.stat('results.txt', function(err, stat){
+  // results.txt文件是否存在
+  if(stat&&stat.isFile()) {
+    console.log('results.txt存在');
+    // 存在则删除
+    fs.unlink('results.txt', function (err) {
+      if (err) {
+        console.log(err);
+      }
+      console.log('results.txt删除成功');
+      fileDisplay(filePath);
+    });
+  } else {
+    console.log('results.txt不存在');
+    fileDisplay(filePath);
+  }
+});
 
 function fileDisplay(filePath) {
-  //根据文件路径读取文件，返回文件列表
+  // 根据文件路径读取文件，返回文件列表
   fs.readdir(filePath, (err, files) => {
     if (err) {
       console.warn(err)
     } else {
-      //遍历读取到的文件列表
+      // 遍历读取到的文件列表
       files.forEach((filename) => {
-        //获取当前文件的绝对路径
+        // 获取当前文件的绝对路径
         const filedir = path.join(filePath, filename);
-        //根据文件路径获取文件信息，返回一个fs.Stats对象
+        // 根据文件路径获取文件信息，返回一个fs.Stats对象
         fs.stat(filedir, (eror, stats) => {
           if (eror) {
             console.warn('获取文件stats失败');
           } else {
-            const isFile = stats.isFile();//是文件
-            const isDir = stats.isDirectory();//是文件夹
+            const isFile = stats.isFile();// 是文件
+            const isDir = stats.isDirectory();// 是文件夹
             if (isFile) {
               checkFiles(filedir);
             }
             if (isDir) {
-              fileDisplay(filedir);//递归，如果是文件夹，就继续遍历该文件夹下面的文件
+              fileDisplay(filedir);// 递归，如果是文件夹，就继续遍历该文件夹下面的文件
             }
           }
         })
